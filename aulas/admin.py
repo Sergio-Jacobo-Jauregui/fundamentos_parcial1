ab79ad2c99ff80a3c0d31a85b28aae17d8c50963
Sergio Jacobo Jauregui <serggiovanni@gmail.com>
2025-01-18 13:52:48 -0500

    anadir verificacion de horarios

--- aulas/admin.py
+++ aulas/admin.py
@@ -13,6 +13,20 @@ class CursoAdmin(admin.ModelAdmin):
     def nombre_completo(self, obj):
         return str(obj)
 
+class CursoAulaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre_completo', 'dia_completo', 'hora_completo')
+    fields = ('curso', 'aula', 'hora_inicio', 'duracion', 'dia')
+
+    def nombre_completo(self, obj):
+        return str(obj)
+
+    def dia_completo(self, obj):
+        dias = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
+        return f"{str(obj.dia)}: {dias[obj.dia - 1]}"
+
+    def hora_completo(self, obj):
+        return f"{str(obj.hora_inicio)} - {obj.hora_fin}"
+
 admin.site.register(Aula, AulaAdmin)
 admin.site.register(Curso, CursoAdmin)
-admin.site.register(CursoAula)
+admin.site.register(CursoAula, CursoAulaAdmin)
--- aulas/models.py
+++ aulas/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from datetime import datetime, timedelta
+from datetime import time
 from django.dispatch import receiver
 from ciclos.models import Ciclo
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
 
 class Aula(models.Model):
     nombre = models.CharField(max_length=2, help_text="Letra del aula.")
@@ -26,18 +28,39 @@ class CursoAula(models.Model):
     aula = models.ForeignKey(Aula, on_delete=models.CASCADE, related_name="curso_aulas")
     hora_inicio = models.TimeField(max_length=100, help_text="Hora de inicio de este curso")
     duracion = models.DecimalField(max_digits=3, decimal_places=1, help_text="Duración del curso en horas", validators=[MinValueValidator(0), MaxValueValidator(7)])
-    hora_fin = models.TimeField(max_length=100, help_text="Hora de fin de este curso")
+    hora_fin = models.TimeField(max_length=100, null=True, blank=True, help_text="Hora de fin de este curso")
+    dia = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(7)])
 
     def __str__(self):
         return f"Curso: {self.curso.nombre}, Aula: {self.aula.nombre}"
 
-    def save(self, *args, **kwargs):
-        print(2222)
-        hora_inicio_raw = datetime.strptime(self.hora_inicio, "%H:%M:%S").time()
-        hora_inicio_dt = datetime.combine(datetime.today(), hora_inicio_raw)
+    def set_hora_fin(self):
+        if issubclass(type(self.hora_inicio), time):
+            self.hora_inicio =  self.hora_inicio.isoformat()
 
-        horas_a_agregar = timedelta(hours=self.duracion)
-        nueva_hora = hora_inicio_dt + horas_a_agregar
+        hora_inicio_datetime = datetime.strptime(self.hora_inicio, "%H:%M:%S").time()
+        hora_inicio_total = datetime.combine(datetime.today(), hora_inicio_datetime)
+
+        duracion_float = float(self.duracion)
+        horas_a_agregar = timedelta(hours=duracion_float)
+        nueva_hora = hora_inicio_total + horas_a_agregar
         self.hora_fin = nueva_hora.time()
 
+    def verificar_cruce_de_horario(self):
+        cursos_conflicto = CursoAula.objects.filter(
+            hora_inicio__lt=self.hora_fin,
+            hora_fin__gt=self.hora_inicio,
+            dia=self.dia,
+            aula=self.aula,
+        )
+
+        if cursos_conflicto.exists():
+            txt = ''
+            for i in cursos_conflicto:
+                txt += f'El curso {i} inicia a las {i.hora_inicio} y termina a las {i.hora_fin}\n'
+
+            raise ValidationError(f'El horario del curso se cruza con otros cursos existentes.\n' + txt)
+    def save(self, *args, **kwargs):
+        self.set_hora_fin()
+        self.verificar_cruce_de_horario()
         super().save(*args, **kwargs)
